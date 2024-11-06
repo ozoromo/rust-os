e5068a24e1a6652ccab30c265b949f8a21cd5420
ozoromo <dev@ozoromo.com>
2024-11-06 17:03:30 +0100

    Println now works

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![no_main]
 
+use core::fmt::Write;
 use core::panic::PanicInfo;
 mod vga_buffer;
 
@@ -13,7 +14,7 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    vga_buffer::print_test();
-
+    println!("Hello {}", "world!");
+    println!(";)");
     loop {}
 }
