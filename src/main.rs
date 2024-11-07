4c6900ca3db466aa20d13f4e1715f08029c97e36
ozoromo <dev@ozoromo.com>
2024-11-07 01:39:02 +0100

    Added tests :)

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,30 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
-use core::fmt::Write;
 use core::panic::PanicInfo;
+use exit_device::{exit_qemu, QuemuExitCode};
+mod exit_device;
+mod serial;
 mod vga_buffer;
 
 static HELLO: &[u8] = b"Hello World!";
 
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[Error]");
+    serial_println!("Panic info:\n\n{}", info);
+    exit_qemu(QuemuExitCode::Failed);
     loop {}
 }
 
@@ -17,5 +32,39 @@ fn panic(info: &PanicInfo) -> ! {
 pub extern "C" fn _start() -> ! {
     println!("Hello {}", "world!");
     println!(";)");
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
+}
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Testable]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test.run();
+    }
+
+    exit_qemu(QuemuExitCode::Success);
+}
+
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) -> () {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+
+#[test_case]
+fn sanity_check() {
+    assert_eq!(1, 1);
 }
